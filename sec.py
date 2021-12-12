a68fbbb8d77d7a5ed691ce19aa407086d164fb78
Felipe Valenzuela <nosmofui@gmail.com>
2021-12-12 01:41:23 -0300

    correccion de errores

--- sec.py
+++ sec.py
@@ -70,7 +70,7 @@ def obtener_sellos():
     for cliente in range(int(os.environ.get('CODIGO_INICIAL')),9999999999999+1):
         busca_producto(cliente)
 
-sched = BlockingScheduler(timezone="Europe/London")
+sched = BlockingScheduler(timezone="America/Santiago")
 sched.add_job(obtener_sellos, 'cron', day_of_week='sun', hour=4, minute=43)
 # sched.add_job(obtener_sellos, 'cron', day_of_week='sun', hour=4, minute=10)
-sched.initialize()
+sched.start()
