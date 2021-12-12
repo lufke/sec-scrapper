165e5d21adb54a8a3cf296d861678e7f62117eb6
Felipe Valenzuela <nosmofui@gmail.com>
2021-12-12 01:36:51 -0300

    cambio horario

--- sec.py
+++ sec.py
@@ -71,6 +71,6 @@ def obtener_sellos():
         busca_producto(cliente)
 
 sched = BlockingScheduler(timezone="Europe/London")
-sched.add_job(obtener_sellos, 'cron', day_of_week='sun', hour=4, minute=25)
+sched.add_job(obtener_sellos, 'cron', day_of_week='sun', hour=4, minute=43)
 # sched.add_job(obtener_sellos, 'cron', day_of_week='sun', hour=4, minute=10)
 sched.initialize()
